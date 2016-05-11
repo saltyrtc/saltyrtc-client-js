b2771c5678ee1799f69afb186ed1249a0bb51945
Danilo Bargen <danilo.bargen@threema.ch>
2016-05-11 14:44:27 +0200

    Revert "use strict"
    This reverts commit 60956f7de3b5fa19d14bb8a81e164a318b03a62a.

--- saltyrtc/main.ts
+++ saltyrtc/main.ts
@@ -6,6 +6,4 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the `LICENSE.md` file for details.
  */
-'use strict';
-
 export { Client } from "./client";
--- tests/main.ts
+++ tests/main.ts
@@ -1,5 +1,3 @@
-'use strict';
-
 import test_utils from "./utils.spec";
 
 test_utils();
