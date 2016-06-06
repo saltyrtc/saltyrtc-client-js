01026538e489a20738184ebbb269dc6cb907c1e7
Danilo Bargen <mail@dbrgn.ch>
2016-06-06 15:27:36 +0200

    Testing: Add connection speed test

--- tests/integration.spec.ts
+++ tests/integration.spec.ts
@@ -35,12 +35,12 @@ export default () => { describe('Integration Tests', () => {
             expect(this.responder.state).toEqual('new');
             this.initiator.connect();
             expect(this.initiator.state).toEqual('ws-connecting');
-            await sleep(1500);
+            await sleep(1000);
             expect(this.initiator.state === 'server-handshake' ||
                    this.initiator.state === 'peer-handshake').toBe(true);
             this.responder.connect();
             expect(this.responder.state).toEqual('ws-connecting');
-            await sleep(1500);
+            await sleep(1000);
             expect(this.initiator.state).toBe('open');
             expect(this.responder.state).toBe('open');
             done();
@@ -51,15 +51,37 @@ export default () => { describe('Integration Tests', () => {
             expect(this.responder.state).toEqual('new');
             this.responder.connect();
             expect(this.responder.state).toEqual('ws-connecting');
-            await sleep(1500);
+            await sleep(1000);
             expect(this.responder.state === 'server-handshake' ||
                    this.responder.state === 'peer-handshake').toBe(true);
             this.initiator.connect();
             expect(this.initiator.state).toEqual('ws-connecting');
-            await sleep(1500);
+            await sleep(1000);
             expect(this.initiator.state).toBe('open');
             expect(this.responder.state).toBe('open');
             done();
+        });
+
+        it('connect speed', async (done) => {
+            expect(this.initiator.state).toEqual('new');
+            expect(this.responder.state).toEqual('new');
+            let t1, t2;
+            let ready = 0;
+            let callback = () => {
+                ready += 1;
+                if (ready == 2) {
+                    t2 = new Date();
+                    let diffMs = t2 - t1;
+                    console.info('Full handshake took', diffMs, 'milliseconds');
+                    expect(diffMs).toBeLessThan(1000);
+                    done();
+                }
+            };
+            this.initiator.onConnected = callback;
+            this.responder.onConnected = callback;
+            t1 = new Date();
+            this.initiator.connect();
+            this.responder.connect();
         });
 
     });
