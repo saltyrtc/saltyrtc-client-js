30c661ebc52cdd9480d40374b16c598ecff0e401
Danilo Bargen <danilo.bargen@threema.ch>
2016-06-07 13:34:41 +0200

    Types: Add Sdp message type

--- saltyrtc/messages.d.ts
+++ saltyrtc/messages.d.ts
@@ -3,7 +3,7 @@ declare namespace saltyrtc {
     type MessageType = 'server-hello' | 'client-hello' | 'client-auth'
                      | 'server-auth' | 'new-initiator' | 'new-responder'
                      | 'drop-responder' | 'send-error' | 'token' | 'key'
-                     | 'auth' | 'offer' | 'answer' | 'candidates' | 'restart';
+                     | 'auth' | 'offer' | 'answer' | 'sdp' | 'candidates' | 'restart';
 
     interface Message {
         type: MessageType,
@@ -79,13 +79,19 @@ declare namespace saltyrtc {
     // https://github.com/saltyrtc/saltyrtc-meta/blob/master/Protocol.md#offer
     interface Offer extends Message {
         type: 'offer',
-        sdp: string, // TODO: #28
+        sdp: string,
     }
 
     // https://github.com/saltyrtc/saltyrtc-meta/blob/master/Protocol.md#answer
     interface Answer extends Message {
         type: 'answer',
-        sdp: string, // TODO: #28
+        sdp: string,
+    }
+
+    interface Sdp extends Message {
+        type: 'sdp',
+        sdp_type: 'offer' | 'answer' | 'pranswer' | 'rollback',
+        sdp: string,
     }
 
     // https://github.com/saltyrtc/saltyrtc-meta/blob/master/Protocol.md#candidates
