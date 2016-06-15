2f89b22760844d87a04e3a6d760252cd69057c92
Danilo Bargen <danilo.bargen@threema.ch>
2016-06-15 10:33:30 +0200

    Remove all traces of Angular
    Fixes #1

--- saltyrtc/datachannel.ts
+++ saltyrtc/datachannel.ts
@@ -5,7 +5,6 @@
  * of the MIT license.  See the `LICENSE.md` file for details.
  */
 
-/// <reference path="types/angular.d.ts" />
 /// <reference path="types/RTCPeerConnection.d.ts" />
 
 import { KeyStore, Box } from "./keystore";
