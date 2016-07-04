9d5abca4b1e5fd47732c3174c2ca83f8eec2f423
Danilo Bargen <danilo.bargen@threema.ch>
2016-07-04 10:38:48 +0200

    Include Babel ES6 polyfill
    The poyfill is needed to support some of the ES6 features in ES5.
    
    This increases the dist size from 30KiB to 120KiB. We should probably
    try to remove the parts of the polyfill that we don't need.

--- saltyrtc/main.ts
+++ saltyrtc/main.ts
@@ -6,6 +6,8 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the `LICENSE.md` file for details.
  */
+import "../../node_modules/babel-polyfill/dist/polyfill";
+
 export { SaltyRTC } from "./client";
 export { KeyStore, Box } from "./keystore";
 export { SecureDataChannel } from "./datachannel";
