8ffb59b7f00fcc533772faaed86fc10a986e2397
Danilo Bargen <danilo.bargen@threema.ch>
2016-07-06 16:08:00 +0200

    Make rollup_tests work

--- saltyrtc/main.ts
+++ saltyrtc/main.ts
@@ -6,7 +6,7 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the `LICENSE.md` file for details.
  */
-import "../../node_modules/babel-polyfill/dist/polyfill";
+import "../node_modules/babel-polyfill/dist/polyfill";
 
 export { SaltyRTC } from "./client";
 export { KeyStore, Box } from "./keystore";
