0ba30ec9528782135d9a79984da1073bfd1bb827
Danilo Bargen <danilo.bargen@threema.ch>
2016-10-10 15:42:54 +0200

    Add PingPongTask

--- tests/testtasks.ts
+++ tests/testtasks.ts
@@ -2,8 +2,8 @@ export class DummyTask implements saltyrtc.Task {
 
     public initialized = false;
     public peerData: Object;
-    private signaling: saltyrtc.Signaling;
-    private name: string;
+    protected signaling: saltyrtc.Signaling;
+    protected name: string;
 
     constructor(name?: string) {
         if (name === undefined) {
@@ -45,4 +45,45 @@ export class DummyTask implements saltyrtc.Task {
         // Do nothing
     }
 
+}
+
+export class PingPongTask extends DummyTask {
+
+    public sentPong = false;
+    public receivedPong = false;
+
+    constructor() {
+        super('pingpong.tasks.saltyrtc.org');
+    }
+
+    getSupportedMessageTypes(): String[] {
+        return ['ping', 'pong'];
+    }
+
+    onPeerHandshakeDone(): void {
+        if (this.signaling.role == 'initiator') {
+            this.sendPing();
+        }
+    }
+
+    sendPing(): void {
+        console.log('Sending ping');
+        this.signaling.sendTaskMessage({'type': 'ping'});
+    }
+
+    sendPong(): void {
+        console.log('Sending pong');
+        this.signaling.sendTaskMessage({'type': 'pong'});
+        this.sentPong = true;
+    }
+
+    onTaskMessage(message: saltyrtc.messages.TaskMessage): void {
+        if (message.type === 'ping') {
+            console.log('Received ping');
+            this.sendPong();
+        } else if (message.type === 'pong') {
+            console.log('Received pong');
+            this.receivedPong = true;
+        }
+    }
 }
