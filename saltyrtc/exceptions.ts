d79a033282dd9956fe5067ee43cbc7fe74f4d114
Danilo Bargen <danilo.bargen@threema.ch>
2016-10-10 10:39:11 +0200

    New exceptions

--- saltyrtc/exceptions.ts
+++ saltyrtc/exceptions.ts
@@ -5,6 +5,9 @@
  * of the MIT license.  See the `LICENSE.md` file for details.
  */
 
+/**
+ * @deprecated Use SignalingError instead
+ */
 export function ProtocolError(message: string) {
     this.message = message;
 	// Use V8's native method if available, otherwise fallback
@@ -18,6 +21,9 @@ ProtocolError.prototype = Object.create(Error.prototype);
 ProtocolError.prototype.name = 'ProtocolError';
 ProtocolError.prototype.constructor = ProtocolError;
 
+/**
+ * @deprecated Use SignalingError instead
+ */
 export function InternalError(message: string) {
     this.message = message;
 	// Use V8's native method if available, otherwise fallback
@@ -30,3 +36,30 @@ export function InternalError(message: string) {
 InternalError.prototype = Object.create(Error.prototype);
 InternalError.prototype.name = 'InternalError';
 InternalError.prototype.constructor = InternalError;
+
+
+/**
+ * A SaltyRTC signaling error.
+ *
+ * It will result in the connection closing with the specified error code.
+ */
+class SignalingError extends Error {
+    public closeCode: number;
+    constructor(closeCode: number, message: string) {
+        super(message);
+        this.message = message;
+        this.closeCode = closeCode;
+        this.name = 'SignalingError';
+    }
+}
+
+/**
+ * Errors related to the network connection state.
+ */
+class ConnectionError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.message = message;
+        this.name = 'ConnectionError';
+    }
+}
