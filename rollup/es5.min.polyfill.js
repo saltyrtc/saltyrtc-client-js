19a6aad19f018593e7f89d51f47b11502e8ff9f6
Danilo Bargen <danilo.bargen@threema.ch>
2016-10-19 15:13:53 +0200

    Fix filename of polyfilled dist file

--- rollup/es5.min.polyfill.js
+++ rollup/es5.min.polyfill.js
@@ -1,6 +1,6 @@
 import config from './es5.min.js';
 
 config.entry = 'src/main.es5.ts';
-config.dest = 'dist/saltyrtc-client.es5.min.rollup.js';
+config.dest = 'dist/saltyrtc-client.es5.min.polyfill.js';
 
 export default config;
